ae9e2141d5dc8c0259d38b74d2416b77e7720e07
kevi2796 <59027271+kevi2796@users.noreply.github.com>
2020-02-12 03:00:04 +0100

    Danish translation

---
+++ locales/dk.lua
@@ -0,0 +1,114 @@
+-Locales ['dk'] = {
+  -- weed
+  ['weed_pickupprompt'] = 'tryk ~INPUT_CONTEXT~ for at høste ~g~Cannabis~s~ planten.',
+  ['weed_inventoryfull'] = 'du har ikke mere plads i dit inventory til ~g~Cannabis~s~.',
+  ['weed_processprompt'] = 'tryk ~INPUT_CONTEXT~ for at starte ~g~Behandling af Cannabis~s~.',
+  ['weed_processingstarted'] = 'laver ~g~Cannabis~s~ om til ~g~Marihuana~s~...',
+  ['weed_processingfull'] = 'behandling ~r~annulleret~s~ på grund af dit inventory er fuldt!',
+  ['weed_processingenough'] = 'du skal have ~b2 stks~s~ ~g~Cannabis~s~ for at lave dem om.',
+  ['weed_processed'] = 'du har lavet ~b~2 stks~s~ ~g~Cannabis~s~ om til ~b~1 stks~s~ ~g~Marihuana~s~',
+  ['weed_processingtoofar'] = 'din behandling er blevet ~r~annulleret~s~ fordi du kom ud af rækkevidde.',
+  
+  -- meth
+  ['HydrochloricAcid_pickupprompt'] = 'tryk ~INPUT_CONTEXT~ for at tage en ~g~Saltsyre~s~ tønde op.',
+  ['SulfuricAcid_pickupprompt'] = 'tryk ~INPUT_CONTEXT~ for at tage en ~g~Svovlsyre~s~ tønde op.',
+  ['SodiumHydroxide_pickupprompt'] = 'tryk ~INPUT_CONTEXT~ for at tage en ~g~Natriumhydroxid~s~ tønde op.',
+  ['HydrochloricAcid_inventoryfull'] = 'du har ikke mere plads i dit inventory til ~g~Saltsyre~s~.',
+  ['SulfuricAcid_inventoryfull'] = 'du har ikke mere plads i dit inventory til ~g~Svovlsyre~s~.',
+  ['sodium_hydroxide_inventoryfull'] = 'du har ikke mere plads i dit inventory til ~g~Natriumhydroxid~s~.',
+  ['meth_processprompt'] = 'tryk ~INPUT_CONTEXT~ for at starte med at ~g~lave metamfetamin~s~.',
+  ['meth_processingstarted'] = 'laver ~g~metamfetamin.~s~...',
+  ['meth_processingfull'] = 'behandling ~r~annulleret~s~ på grund af dit inventory er fuldt!',
+  ['meth_processingenough'] = 'du skal have ~b~1 stks~s~ af alt for at lave det om.',
+  ['meth_processed'] = 'du har lavet ~b~1 stks~s~ ~g~metamfetamin.~s~',
+  ['meth_processingtoofar'] = 'din behandling er blevet ~r~annulleret~s~ fordi du kom ud af rækkevidde.',
+  
+  -- coke
+  ['coke_pickupprompt'] = 'tryk ~INPUT_CONTEXT~ for at høste ~g~Kokain~s~ planten.',
+  ['coke_inventoryfull'] = 'du har ikke mere plads i dit inventory til ~g~Kokain blade~s~.',
+  ['coke_processprompt'] = 'tryk ~INPUT_CONTEXT~ for at starte ~g~Behandling af Kokain~s~.',
+  ['coke_processingstarted'] = 'laver ~g~Kokain~s~...',
+  ['coke_processingfull'] = 'behandling ~r~annulleret~s~ på grund af dit inventory er fuldt!',
+  ['coke_processingenough'] = 'du skal have ~b~3 stks~s~ ~g~Kokain blade~s~ for at lave dem om.',
+  ['coke_processed'] = 'du har lavet ~b~1 stks~s~ ~g~Kokain~s~',
+  ['coke_processingtoofar'] = 'din behandling er blevet ~r~annulleret~s~ fordi du kom ud af rækkevidde.',
+  
+  -- lsd
+  ['lsd_processingstarted'] = 'laver ~g~Lyserginsyre diethylamid~s~...',
+  ['lsd_processingfull'] = 'behandling ~r~annulleret~s~ på grund af dit inventory er fuldt!',
+  ['lsd_processprompt'] = 'tryk ~INPUT_CONTEXT~ for at starte ~g~Behandling af Lyserginsyre diethylamid~s~.',
+  ['lsd_processingenough'] = 'du skal have ~b~1 stks~s~ ~g~Lysergsyreamid~s~ og ~b~1 stks~s~ ~g~Thionyl chlorid~s~ for at lave dem om.',
+  ['lsd_processed'] = 'du har lavet ~b~1 stks~s~ ~g~Lyserginsyre diethylamid~s~',
+  ['lsd_processingtoofar'] = 'din behandling er blevet ~r~annulleret~s~ fordi du kom ud af rækkevidde.',
+  
+  ['thionylchloride_processingstarted'] = 'laver ~g~Thionyl Chloride~s~...',
+  ['thionylchloride_processingfull'] = 'behandling ~r~annulleret~s~ på grund af dit inventory er fuldt!',
+  ['thionylchloride_processprompt'] = 'tryk ~INPUT_CONTEXT~ for at starte ~g~Behandling af Thionyl Chloride~s~.',
+  ['thionylchloride_processingenough'] = 'du skal have ~b~1 stks~s~ ~g~Lysergsyreamid~s~ og ~b~1 stks~s~ ~g~Kemikalier~s~ for at lave dem om.',
+  ['thionylchloride_processed'] = 'du har lavet ~b~1 stks~s~ ~g~Thionyl Chloride~s~',
+  ['thionylchloride_processingtoofar'] = 'din behandling er blevet ~r~annulleret~s~ fordi du kom ud af rækkevidde.',
+  
+   -- heroin
+  ['heroin_pickupprompt'] = 'tryk ~INPUT_CONTEXT~ for at høste ~g~Poppy~s~ planten.',
+  ['poppy_inventoryfull'] = 'du har ikke mere plads i dit inventory til ~g~Poppy Resin~s~.',
+  ['heroin_inventoryfull'] = 'du har ikke mere plads i dit inventory til ~g~Heroin~s~.',
+  ['heroin_processprompt'] = 'tryk ~INPUT_CONTEXT~ for at starte ~g~Behandling af Heroin~s~.',
+  ['heroin_processingstarted'] = 'laver ~g~Poppy Resin~s~ om til ~g~Heroin~s~...',
+  ['heroin_processingfull'] = 'behandling ~r~annulleret~s~ på grund af dit inventory er fuldt!',
+  ['heroin_processingenough'] = 'du skal have ~b~1 stks~s~ ~g~Poppy Resin~s~ for at lave det om.',
+  ['heroin_processed'] = 'du har lavet ~b~1 stks~s~ ~g~Heroin~s~',
+  ['heroin_processingtoofar'] = 'din behandling er blevet ~r~annulleret~s~ fordi du kom ud af rækkevidde.',
+  
+  -- chemicals
+  ['chemicals_pickupprompt'] = 'tryk ~INPUT_CONTEXT~ for at tage dine ~g~Kemikalier~s~',
+  ['chemicals_inventoryfull'] = 'du har ikke mere plads i dit inventory til ~g~Kemikalier~s~.',
+  ['Chemicals_notenough'] = 'du har ikke flere ~g~Kemikalier~s~ at lave om.',
+  ['Chemicals_made'] = 'du har lavet ~b~1 stks~s~ ~y~%s~s~',
+  ['chemicals_prompt'] = 'tryk ~INPUT_CONTEXT~ for at åbne ~r~Kemisk konverteringsmenu~s~.',
+  ['ChemicalsConvertion_title'] = 'Kemisk konverteringsmenu',
+  ['chemicals_making'] = '~g~Laver...~s~',
+  
+  -- drug dealer
+  ['dealer_prompt'] = 'tryk ~INPUT_CONTEXT~ for at snakke med ~r~Pusheren~s~.',
+  ['dealer_title'] = 'Pusher',
+  ['dealer_item'] = '$%s',
+  ['dealer_notenough'] = 'Du har ikke nok til at sælge!',
+  ['dealer_sold'] = 'Du har solgt ~b~%sx~s~ ~y~%s~s~ for ~g~$%s~s~',
+
+  -- license
+  ['license_title'] = 'Du er forpligtet til at eje et behandlingslicens for at lave stoffer, det kan købes her.',
+  ['license_no'] = 'nej',
+  ['license_bought'] = 'du købte ~b~%s~s~ for ~r~$%s~s~',
+  ['license_bought_fail'] = 'du har ikke råd til en ~b~%s~s~!',
+  ['license_weed'] = 'Cannabis behandlings licens',
+  ['license_meth'] = 'Metamfetamin behandlings licens',
+  ['need_license'] = 'Du har brug for en licens for at bruge denne.',
+  ['licenseshop_title'] = 'Licens Butik',
+  ['licenseshop_prompt'] = 'tryk ~INPUT_CONTEXT~ for at åbne ~g~Licens Butikken~s~.',
+  ['license_bought'] = 'du købte ~b~1 stks~s~ ~y~%s~s~ for ~g~$%s~s~',
+  ['license_inventoryfull'] = 'du har allerede denne licens på dig.',
+  
+  -- moneywash
+  ['moneywash_washed'] = 'du har hvidvasket ~r~$%s~s~',
+  ['moneywash_title'] = 'Hvidvask',
+  ['moneywash_wash'] = 'Hvidvask dine sortepenge',
+  ['moneywash_prompt'] = 'tryk ~INPUT_CONTEXT~ for at åbne ~r~Hvidvask~s~.',
+  
+  -- blips
+  ['blip_weedfield'] = 'Cannabis Mark',
+  ['blip_weedprocessing'] = 'Cannabis Behandlingscenter',
+  ['blip_methfield'] = 'Metamfetamin Mark',
+  ['blip_methprocessing'] = 'Metamfetamin Behandlingscenter',
+  ['blip_drugdealer'] = 'Pusher',
+  ['blip_ChemicalsProcessing'] = 'Kemikalier Behandlingscenter',
+  ['blip_ChemicalsFarm'] = 'Kemikalier Mark',
+  ['blip_CokeFarm'] = 'Kokain Mark',
+  ['blip_Cokeprocessing'] = 'Kokain Behandlingscenter',
+  ['blip_herionfield'] = 'Heroin Mark',
+  ['blip_heroinprocessing'] = 'Heroin Behandlingscenter',
+  ['blip_HydrochloricAcidFarm'] = 'Saltsyre Mark',
+  ['blip_lsdprocessing'] = 'Lyserginsyre-diethylamid Behandlingscenter',
+  ['blip_SodiumHydroxideFarm'] = 'Natriumhydroxid Mark',
+  ['blip_SulfuricAcidFarm'] = 'Svovlsyre Mark',
+}
+
